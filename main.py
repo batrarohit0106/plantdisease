3894fa35b1837eeee2237e4fb31cb4aa1761cc7d
batrarohit0106 <135214621+batrarohit0106@users.noreply.github.com>
2023-06-21 12:24:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import tensorflow as tf
 from fastapi.middleware.cors import CORSMiddleware
 app= FastAPI()
 origins={
-    "http://localhost",
+    "https://plantdisease.vercel.app/",
     "http://localhost:3000"
 }
 app.add_middleware(
